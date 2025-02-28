21c2be19b4699a6d67e0ea1146610068b3809ea8
Shreemay Dixit <shreemaydixit.sd@gmail.com>
2025-02-28 11:07:09 +0530

    Initial commit

---
+++ src/Queue/queue.java
@@ -0,0 +1,26 @@
+package Queue;
+
+import java.util.Queue;
+import java.util.LinkedList;
+public class queue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(7);
+        q.add(10);
+
+        System.out.println(q);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
